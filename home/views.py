6e24e34e360feca6b25d884c3b2dc24f58538575
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2026-04-04 19:07:10 +0400

    Real Estate - Home Search Filter(Back End) v10

--- home/views.py
+++ home/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from propertylisting.models import Properties
+from django.db.models import Q
 
 def homepage(request):
     return render(request, "home/home.html")
 
 def search_filter(request):
-    print(request)
-    return render(request, "properties.html")
+    listings = Properties.objects.all().order_by("-id")
+
+    #request.GET == it is returning a dictionary
+    #.get() == retrieving key value pairs. Eg: key = location, value = location value
+    #<QueryDict: {'location': ['Bamboo'], 'min_price': [''], 'max_price': [''], 'keywords': ['']}> 
+    location = request.GET.get("location") 
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+    keywords = request.GET.get("keywords")
+    
+    if location:
+        listings = listings.filter(location__icontains=location)  #If the user entered a location, return all listings whose location contains that text (ignoring uppercase/lowercase)
+
+    if min_price:
+        listings = listings.filter(price__gte=min_price) #gte - greater than or equal to
+    
+    if max_price:
+        listings = listings.filter(price__lte=max_price) #lte - less than or equal to
+    
+    if keywords:
+        listings = Properties.objects.filter(Q(title__icontains=keywords) | Q(description__icontains=keywords) | Q(location__icontains=keywords))
+    return render(request, "properties.html", {"properties_list": listings})
     
 
--- propertylisting/models.py
+++ propertylisting/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Properties(models.Model):
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
-    price_per_month = models.DecimalField(max_digits=12, decimal_places=2)
+    price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField()
     owner = models.CharField(max_length=200)
