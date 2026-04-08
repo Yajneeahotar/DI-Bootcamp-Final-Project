eca0efa4e7e7ebf3b89fb66f1794a33a00f1a65c
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2026-04-08 19:17:53 +0400

    Real Estate - Register Form v19

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
 
 def login(request):
     return render(request, "login.html")
 
+def register_view(request):
+    if request.user.is_authenticated:
+       return redirect("homepage")  
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("USER CREATED")  
+        else:
+            print(form.errors)  
+
+            
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request,
+                f"Account created for {username}! You can now log in."
+            )
+            return redirect("login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
