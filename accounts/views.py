fd8f8ee779521aee77cdadf4a546e85f64057e76
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2026-04-09 18:49:31 +0400

    Real Estate - Register Form(Back end) - v20

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login
 
-def login(request):
-    return render(request, "login.html")
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("homepage")
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, f"Welcome back, {username}!")
+            return redirect("homepage")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "login.html", {})
 
 def register_view(request):
     if request.user.is_authenticated:
@@ -13,11 +29,7 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            print("USER CREATED")  
-        else:
-            print(form.errors)  
-
-            
+           
             username = form.cleaned_data.get("username")
             messages.success(
                 request,
