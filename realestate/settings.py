7b63feed79f8f84258654607d90a01970229f7b5
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2026-04-20 21:50:26 +0400

    Real Estate - Settings File(deployment) v30

--- realestate/settings.py
+++ realestate/settings.py
@@ -85,11 +85,31 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+
+DATABASES = {
+
+'default': {
+
+    'ENGINE': 'django.db.backends.postgresql',
+
+    'NAME': os.environ.get('DB_NAME'),
+
+    'USER': os.environ.get('DB_USER'),
+
+    'PASSWORD': os.environ.get('DB_PASSWORD'),
+
+    'HOST': os.environ.get('DB_HOST'),
+
+    'PORT': '5432',
+
+}
+
 }
 
 
